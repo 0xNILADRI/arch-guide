d4ea87ec719aba81b6f1605d3434154c1091998c
Niladri Ghosh <31654012+afkniladri@users.noreply.github.com>
2021-09-06 15:00:02 +0530

    Add files via upload

---
+++ base-uefi.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
+
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+# You can remove the tlp package if you are installing on a desktop or vm
+
+pacman -S efibootmgr networkmanager network-manager-applet dialog iwd mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio pipewire pipewire-alsa pipewire-jack pavucontrol bash-completion openssh rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+
+# pacman -S --noconfirm xf86-video-amdgpu
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+#grub-mkconfig -o /boot/grub/grub.cfg
+
+#systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable libvirtd
+systemctl enable firewalld
+systemctl enable acpid
+
+useradd -m afkniladri
+echo ermanno:password | chpasswd
+usermod -aG libvirt afkniladri
+
+echo "afkniladri ALL=(ALL) ALL" >> /etc/sudoers.d/afkniladri
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
+
+
+
---
+++ gnome.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo reflector -c India -a 2 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Sy
+
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+# sudo virsh net-autostart default
+
+
+sudo pacman -S --noconfirm xorg xorg-server gdm gnome gnome-extra firefox gnome-tweaks simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji archlinux-wallpaper
+
+sudo flatpak install -y spotify
+# sudo flatpak install -y kdenlive
+
+sudo systemctl enable gdm
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+# sleep 5
+# sudo reboot
